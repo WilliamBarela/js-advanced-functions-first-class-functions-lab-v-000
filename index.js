76684982661e66c60c70411cf8ef66c5564f10b0
William Barela <william.barela.dev@gmail.com>
2020-02-12 19:14:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const returnFirstTwoDrivers = (drivers) => {
   return drivers.slice(0,2);
-}
+};
 
 const returnLastTwoDrivers = (drivers) => {
   return drivers.slice(-2)
-}
+};
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
@@ -14,8 +14,12 @@ const createFareMultiplier = (multiplier) => {
   return (fare) => {
     return fare * multiplier
   }
-}
+};
 
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, driverSelector) => {
+  return driverSelector(drivers);
+}
